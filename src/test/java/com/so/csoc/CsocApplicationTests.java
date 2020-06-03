ef98b2694aa7034301a7b66259053dff2d77a76a
Artem Nikulin <artem.nikulin@davita.com>
2020-06-03 17:48:40 +0300

    Initial commit

---
+++ src/test/java/com/so/csoc/CsocApplicationTests.java
@@ -0,0 +1,14 @@
+package com.so.csoc;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
